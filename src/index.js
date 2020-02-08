1b8c8cc2e5d6f2f82af5ca68839f42845471109d
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-02-08 12:48:23 -0500

    Adding in stock files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*
+1. Check to see if uploading an edited file causes a git diff
+Check a list of all changed files
+    * Added
+    * Modified
+    * Deleted
+    * Moved
+        * Is this two calls to S3?
+        * One to delete, one to add
+Loop over list and do correct action to load to S3
+    * All files should be public
+*/
+
+let thing = "taco";
+
+return thing;
