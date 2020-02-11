7f8968bc535e07bd8d0a0c25ed3ea58bba8c7894
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-02-10 19:58:07 -0500

    Updating the script name and removing dead comments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 /* eslint-disable no-console */
-/*
-DONE Check to see if uploading an edited file causes a git diff
-DONE Check a list of all changed files
-    * Added
-    * Modified
-    * Deleted
-    * Moved
-        * Is this two calls to S3?
-        * One to delete, one to add
-Loop over list and do correct action to load to S3
-    * All files should be public
-*/
 const getChangedFiles = require("./get-changed-files");
 const cdnManager = require("./cdn-manager");
 
 const submitChangedFilesToCDN = async () => {
   const changedFiles = await getChangedFiles();
-  // console.log(changedFiles);
-  // const [firstFile] = changedFiles;
-  // console.log(firstFile);
-  // await cdnManager.addFile(firstFile.filename);
-  // await cdnManager.removeFile(firstFile.filename);
+
   changedFiles.forEach(async ({ filename, status }) => {
     if (["modified", "added"].includes(status.toLowerCase())) {
       await cdnManager.modifyFile(filename);
