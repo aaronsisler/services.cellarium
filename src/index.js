19a67b9d7843170499c7dfde6898c6473fa53dc3
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-02-22 15:43:47 -0500

    Adding in the final package for git-my-files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 /* eslint-disable no-console */
 const getChangedFiles = require("./get-changed-files");
-// const cdnManager = require("./cdn-manager");
+const cdnManager = require("./cdn-manager");
 
 const submitChangedFilesToCDN = async () => {
-  const changedFiles = await getChangedFiles("./assets");
+  const changedFiles = getChangedFiles("./assets");
 
-  console.log(changedFiles);
-  // changedFiles.forEach(async ({ filename, status = "deleted" }) => {
-  //   console.log(filename);
-  //   console.log(status);
-  //   if (["modified", "added"].includes(status.toLowerCase())) {
-  //     // await cdnManager.modifyFile(filename);
-  //   } else if (status.toLowerCase() === "deleted") {
-  //     // await cdnManager.removeFile(filename);
-  //   } else {
-  //     console.log("File status not found", status, filename);
-  //   }
-  // });
+  changedFiles.forEach(async ({ filename, status = "deleted" }) => {
+    if (["modified", "added"].includes(status.toLowerCase())) {
+      await cdnManager.modifyFile(filename);
+    } else if (status.toLowerCase() === "deleted") {
+      await cdnManager.removeFile(filename);
+    } else {
+      console.log("File status not found", status, filename);
+    }
+  });
 };
 
 submitChangedFilesToCDN();
